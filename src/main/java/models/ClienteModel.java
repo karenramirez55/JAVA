ba0971e849f8f7e30c211e8088f512977d28a856
unknown <ramirezkarendenise@gmail.com>
2024-09-24 00:23:37 +0200

    update

---
+++ src/main/java/models/ClienteModel.java
@@ -0,0 +1,66 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CLIENTE")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "email")
+    private String email;
+
+    // Getters y setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CLIENTE (direccion,nombre,apellido,email) values
+("Via 24 maggio 62","Karen Denise","Ramirez","kar@gmail.com"),
+("Via piave 20","Carla Nazareth","Trovato","car@gmail.com"),
+("Calle 140 Berazategui","Lucas","Silva","lucas@gmail.com");
